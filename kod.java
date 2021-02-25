0859bf1fbb960b0ef7f3352f95a425b2fe56840b
Piotrek779 <73425199+Piotrek779@users.noreply.github.com>
2021-02-25 13:26:20 +0100

    Add files via upload

---
+++ kod.java
@@ -0,0 +1,19 @@
+package zadaniadzis;
+
+public class kod {
+    public static void main(String[] args) {
+        // napisz program kotry oblicza sume a^2 + b^2 wypisz wynik
+    int myIntA = 2;
+    int myIntB = 8;
+    int dod = myIntA * myIntA + myIntB * myIntB;
+        System.out.println("wynik wynosi: " + dod);
+
+        //napisz tekst wsztstkie zmienne
+        System.out.println("dzisiaj poznalismy: int, double, char, string ");
+
+        // wypisz imie nazwisko stringami
+        String mystringA = "Piotr";
+        String mystringB = "Balcerski";
+        System.out.println(mystringA + "\n\n" + mystringB);
+    }
+}
